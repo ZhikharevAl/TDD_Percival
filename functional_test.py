69976f4c3a5fb6462c363910feec1cfa03c2e0b6
AlexeyZh <waltafunk@gmail.com>
2022-11-01 09:34:45 +0300

    init

--- functional_test.py
+++ functional_test.py
@@ -1,11 +1,15 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from packaging import version
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.maximize_window()
-base_url = "https://localhost:8080"
-time.sleep(2)
-assert 'Django' in driver.title
+base_url = "http://127.0.0.1:8000/"
+driver.get(base_url)
+title = driver.find_element(By.CSS_SELECTOR, 'a[class="logo"]').text
+print(title)
+assert title == "django"
+print("Test passed")
